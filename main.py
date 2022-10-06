e369521cb99379ca684e649baebc7c6c28a6461d
Leon Okubo <leon.okubo@fs.com.br>
2022-10-06 15:13:36 -0300

    after and before

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-from fastapi import FastAPI
+import time
+
+from fastapi import FastAPI, Request
 
 from app.router.short_url import router
 
@@ -19,3 +21,14 @@ async def on_startup() -> None:
 @app.on_event('shutdown')
 async def on_shutdown() -> None:
     ...
+
+
+@app.middleware("http")
+async def add_process_time_header(request: Request, call_next):
+    start_time = time.time()
+    # after
+    response = await call_next(request)
+    # before
+    process_time = time.time() - start_time
+    response.headers["X-Process-Time"] = str(process_time)
+    return response
